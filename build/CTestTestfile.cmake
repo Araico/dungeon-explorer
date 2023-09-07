d9fd223cae6998123a568add558a95a311880cf6
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2023-09-06 18:20:16 -0600

    Added build folder to gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gatuno7000/Documents/GitHub/dungeon-explorer
-# Build directory: C:/Users/Gatuno7000/Documents/GitHub/dungeon-explorer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
